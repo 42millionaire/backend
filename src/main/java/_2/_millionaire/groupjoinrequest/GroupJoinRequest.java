ba748e1fc3e8703b622cab7f329cef49cc5a337f
soohalee <suha630@naver.com>
2024-09-15 19:26:31 +0900

    [feat] : group,groupJoinRequest,groupMember,member

--- src/main/java/_2/_millionaire/groupjoinrequest/GroupJoinRequest.java
+++ src/main/java/_2/_millionaire/groupjoinrequest/GroupJoinRequest.java
@@ -1,4 +1,28 @@
 package _2._millionaire.groupjoinrequest;
 
-public class GroupJoinRequest {
+import _2._millionaire.BaseEntity;
+import _2._millionaire.group.Group;
+import _2._millionaire.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class GroupJoinRequest extends BaseEntity {
+
+    @ManyToOne()
+    @JoinColumn()
+    Member member;
+
+    @ManyToOne()
+    @JoinColumn()
+    Group group;
 }
