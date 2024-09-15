309b859164e283842be7f25129ec3375e3820165
Hanjjong <tim4974@naver.com>
2024-09-15 17:51:06 +0900

    [feat] : feat Verification image entity

--- src/main/java/_2/_millionaire/verification_image/VerificationImage.java
+++ src/main/java/_2/_millionaire/verification_image/VerificationImage.java
@@ -1,4 +1,23 @@
 package _2._millionaire.verification_image;
 
-public class VerificationImage {
+import _2._millionaire.BaseEntity;
+import _2._millionaire.verification.Verification;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class VerificationImage extends BaseEntity {
+
+    @Lob
+    private byte[] image;
+
+    @ManyToOne()
+    @JoinColumn(name = "verification_id", referencedColumnName = "id")
+    private Verification verification;
+
 }
