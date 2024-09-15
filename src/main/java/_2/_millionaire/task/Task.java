e91d3514f8f341eaa174dc2e59c3712384fb774f
Hanjjong <tim4974@naver.com>
2024-09-15 17:50:39 +0900

    [feat] : feat task entity

--- src/main/java/_2/_millionaire/task/Task.java
+++ src/main/java/_2/_millionaire/task/Task.java
@@ -1,4 +1,34 @@
 package _2._millionaire.task;
 
-public class Task {
+import _2._millionaire.BaseEntity;
+import _2._millionaire.appeal.Appeal;
+import _2._millionaire.verification.Verification;
+import _2._millionaire.verification_image.VerificationImage;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Task extends BaseEntity {
+
+    @Column(length = 10)
+    private String type;
+
+    @Column(length = 300)
+    private String content;
+
+    @Column(length = 20)
+    private String status;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<Appeal> appeals;
+
+    @OneToOne(mappedBy = "task", cascade = CascadeType.ALL)
+    private Verification verification;
 }
