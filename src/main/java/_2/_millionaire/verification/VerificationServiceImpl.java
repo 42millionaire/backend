a3406c18776e450846e5c4c577d733a4b2b26b7c
Hanjjong <tim4974@naver.com>
2024-09-16 00:36:09 +0900

    [feat] : feat createVerification controller, service

--- src/main/java/_2/_millionaire/verification/VerificationServiceImpl.java
+++ src/main/java/_2/_millionaire/verification/VerificationServiceImpl.java
@@ -1,4 +1,10 @@
 package _2._millionaire.verification;
 
+import _2._millionaire.verification.dto.CreateVerificationRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VerificationServiceImpl implements VerificationService{
+    public void createVerification(CreateVerificationRequest createVerificationRequest) {
+    }
 }
