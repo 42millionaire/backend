491d2cb529e35bcf9bd79ada83bf0ea5b2c7f7fb
Hanjjong <tim4974@naver.com>
2024-09-16 03:05:38 +0900

    [feat] : feat verificationImage API

--- src/main/java/_2/_millionaire/verification_image/VerificationImageRepository.java
+++ src/main/java/_2/_millionaire/verification_image/VerificationImageRepository.java
@@ -1,6 +1,8 @@
 package _2._millionaire.verification_image;
 
+import _2._millionaire.verification.Verification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VerificationImageRepository extends JpaRepository<VerificationImage, Long> {
+    void deleteByVerification(Verification verification);
 }
