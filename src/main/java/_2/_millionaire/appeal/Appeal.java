43a709e2a60685e7c2ea44ae01c5c02e445aea03
Hanjjong <tim4974@naver.com>
2024-09-15 17:50:27 +0900

    [feat] : feat appeal entity

--- src/main/java/_2/_millionaire/appeal/Appeal.java
+++ src/main/java/_2/_millionaire/appeal/Appeal.java
@@ -2,9 +2,25 @@ package _2._millionaire.appeal;
 
 
 import _2._millionaire.BaseEntity;
+import _2._millionaire.task.Task;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class Appeal extends BaseEntity {
 
+    @Column(length = 100)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", referencedColumnName = "id")
+    private Task task;
 }
