db6610b05c2f8ca4317ef8dfe5dd890c9919a491
Hanjjong <tim4974@naver.com>
2024-09-15 17:50:58 +0900

    [feat] : feat Verification entity

--- src/main/java/_2/_millionaire/verification/Verification.java
+++ src/main/java/_2/_millionaire/verification/Verification.java
@@ -1,4 +1,28 @@
 package _2._millionaire.verification;
 
-public class Verification {
+import _2._millionaire.BaseEntity;
+import _2._millionaire.task.Task;
+import _2._millionaire.verification_image.VerificationImage;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Verification extends BaseEntity {
+
+    private String content;
+
+    @OneToOne
+    @JoinColumn(name = "task_id", referencedColumnName = "id")
+    private Task task;
+
+    @OneToMany(mappedBy = "verification")
+    private List<VerificationImage> verificationImages;
+
 }
