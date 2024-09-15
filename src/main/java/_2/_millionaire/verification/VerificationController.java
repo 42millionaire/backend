f78f860f1aa852d165d661e8bab0c11349cc4d88
Hanjjong <tim4974@naver.com>
2024-09-16 03:05:47 +0900

    [feat] : feat verification API

--- src/main/java/_2/_millionaire/verification/VerificationController.java
+++ src/main/java/_2/_millionaire/verification/VerificationController.java
@@ -1,13 +1,11 @@
 package _2._millionaire.verification;
 
-import _2._millionaire.verification.dto.CreateVerificationRequest;
+import _2._millionaire.verification.dto.SearchVerificationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/verification")
@@ -17,10 +15,15 @@ public class VerificationController {
     private final VerificationServiceImpl verificationService;
 
     @PostMapping("")
-    public ResponseEntity<String> createVerification(@RequestBody CreateVerificationRequest createVerificationRequest){
-        verificationService.createVerification(createVerificationRequest);
+    public ResponseEntity<String> createVerification(@RequestParam("taskId") Long taskId,
+                                                     @RequestParam("content") String content,
+                                                     @RequestParam(value = "images", required = false) MultipartFile[] images){
+        verificationService.createVerification(taskId, content, images);
         return ResponseEntity.status(HttpStatus.CREATED).body("인증 제출 되었습니다.");
 
     }
-
+    @GetMapping("/{taskId}")
+    public ResponseEntity<SearchVerificationResponse> searchVerification(@PathVariable("taskId") Long taskId){
+        return ResponseEntity.status(HttpStatus.OK).body(verificationService.searchVerification(taskId));
+    }
 }
