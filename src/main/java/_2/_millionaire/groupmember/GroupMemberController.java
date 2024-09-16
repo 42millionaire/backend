66ed1735a60f04ea4f46c20137ebf343b23fe10c
soohalee <suha630@naver.com>
2024-09-16 15:41:08 +0900

    [fix] : groupMember Domain

--- src/main/java/_2/_millionaire/groupmember/GroupMemberController.java
+++ src/main/java/_2/_millionaire/groupmember/GroupMemberController.java
@@ -3,10 +3,7 @@ package _2._millionaire.groupmember;
 import _2._millionaire.groupmember.dto.SearchGroupMemberListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/groupmember")
@@ -15,8 +12,7 @@ public class GroupMemberController {
     private final GroupMemberServiceImpl groupMemberService;
 
     @GetMapping("")
-    public ResponseEntity<SearchGroupMemberListResponse> searchAllGroupMembers(@RequestParam("groupId") Long groupId) {
-        SearchGroupMemberListResponse searchGroupMemberListResponse = groupMemberService.searchAllGroupMembers(groupId);
-        return ResponseEntity.ok(searchGroupMemberListResponse);
+    public ResponseEntity<SearchGroupMemberListResponse> searchAllGroupMembers(@PathVariable("groupId") Long groupId) {
+        return ResponseEntity.ok(groupMemberService.searchAllGroupMembers(groupId));
     }
 }
