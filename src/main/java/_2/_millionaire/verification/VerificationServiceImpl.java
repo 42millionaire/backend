37658a688867b9a62bc03a80758ca13d27d85a68
Hanjjong <tim4974@naver.com>
2024-09-16 21:09:43 +0900

    [feat] : add exception

--- src/main/java/_2/_millionaire/verification/VerificationServiceImpl.java
+++ src/main/java/_2/_millionaire/verification/VerificationServiceImpl.java
@@ -2,8 +2,11 @@ package _2._millionaire.verification;
 
 import _2._millionaire.task.Task;
 import _2._millionaire.task.TaskRepository;
+import _2._millionaire.task.exception.TaskCustomException;
+import _2._millionaire.task.exception.TaskErrorCode;
 import _2._millionaire.verification.dto.SearchVerificationResponse;
-import _2._millionaire.task.dto.UpdateTaskStatusRequest;
+import _2._millionaire.verification.exception.VerificationErrorCode;
+import _2._millionaire.verification.exception.VerificationCustomException;
 import _2._millionaire.verification_image.VerificationImage;
 import _2._millionaire.verification_image.VerificationImageRepository;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +31,7 @@ public class VerificationServiceImpl implements VerificationService{
     @Transactional
     public void createVerification(Long taskId, String content, MultipartFile[] images) {
         Task task = taskRepository.findById(taskId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 task 입니다."));
+                .orElseThrow(() -> new TaskCustomException(TaskErrorCode.TASK_NOT_FOUND));
 
         // Task에 대한 Verification 존재 여부 확인
         Verification verification = verificationRepository.findByTask(task)
@@ -56,7 +59,7 @@ public class VerificationServiceImpl implements VerificationService{
                             .build();
                     verificationImages.add(verificationImage);
                 } catch (IOException e) {
-                    throw new RuntimeException("이미지 저장 중 에러가 발생했습니다.");
+                    throw new VerificationCustomException(VerificationErrorCode.IMAGE_SAVE_ERROR);
                 }
             }
             verificationImageRepository.saveAll(verificationImages);
@@ -65,7 +68,7 @@ public class VerificationServiceImpl implements VerificationService{
 
     public SearchVerificationResponse searchVerification(Long taskId) {
         Verification verification = verificationRepository.findByTaskId(taskId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 task에 대한 인증이 없습니다."));
+                .orElseThrow(() -> new VerificationCustomException(VerificationErrorCode.VERIFICATION_NOT_FOUND));
 
         List<String> base64Images = verification.getVerificationImages().stream()
                 .map(VerificationImage::getBase64Image)
