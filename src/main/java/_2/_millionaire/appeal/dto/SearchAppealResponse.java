4f9c91e7bf947d13fdd36f0cb550b96b03bcea23
Hanjjong <tim4974@naver.com>
2024-09-16 21:08:59 +0900

    [feat] : feat Dto

--- src/main/java/_2/_millionaire/appeal/dto/SearchAppealResponse.java
+++ src/main/java/_2/_millionaire/appeal/dto/SearchAppealResponse.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Builder
 public record SearchAppealResponse(Long taskId,
+                                   Long appealId,
                                    Long memberId,
                                    String memberName,
                                    String content,
--- src/main/java/_2/_millionaire/task/dto/SearchTaskResponse.java
+++ src/main/java/_2/_millionaire/task/dto/SearchTaskResponse.java
@@ -2,11 +2,13 @@ package _2._millionaire.task.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Builder
 public record SearchTaskResponse(Long taskId,
                                  String content,
+                                 LocalDate dueDate,
                                  LocalDateTime createdTime,
                                  LocalDateTime updatedTime,
                                  String status) {
