54e64a8ec3aea86f57e821e6281f8b4892c497e8
Hanjjong <tim4974@naver.com>
2024-09-17 14:44:46 +0900

    [feat] : feat scheduler

--- src/main/java/_2/_millionaire/taskScheduler/TaskScheduler.java
+++ src/main/java/_2/_millionaire/taskScheduler/TaskScheduler.java
@@ -1,11 +1,13 @@
 package _2._millionaire.taskScheduler;
 
+import _2._millionaire.task.Task;
 import _2._millionaire.task.TaskServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +18,12 @@ public class TaskScheduler {
     @Scheduled(cron = "0 0 3 ? * MON-FRI")
     public void checkAndCreatePendingTasks() {
         LocalDate date = LocalDate.now();
-
         taskService.createTaskForTomorrow(date);
+    }
+
+    @Scheduled(cron = "59 59 23 ? * MON-FRI")
+    public void updateTaskStatusToDeny(){
+        LocalDate today = LocalDate.now();
+        taskService.updateTaskStatus(today);
     }
 }
