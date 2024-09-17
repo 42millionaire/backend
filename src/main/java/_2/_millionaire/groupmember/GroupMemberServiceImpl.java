d7bbea5e610b35b12bb60f5784c461ce987bc530
soohalee <suha630@naver.com>
2024-09-17 22:35:26 +0900

    [fix] : joinGroupMember()

--- src/main/java/_2/_millionaire/groupmember/GroupMemberServiceImpl.java
+++ src/main/java/_2/_millionaire/groupmember/GroupMemberServiceImpl.java
@@ -3,6 +3,7 @@ package _2._millionaire.groupmember;
 import _2._millionaire.group.GroupRepository;
 import _2._millionaire.group.Groups;
 import _2._millionaire.groupjoin.GroupJoin;
+import _2._millionaire.groupjoin.GroupJoinRepository;
 import _2._millionaire.groupmember.dto.GroupMemberRequest;
 import _2._millionaire.groupmember.dto.RollGroupMemberRequest;
 import _2._millionaire.group.exception.GroupCustomException;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,6 +30,7 @@ public class GroupMemberServiceImpl implements  GroupMemberSerivce{
     private final GroupRepository groupRepository;
     private final MemberRepository memberRepository;
     private final GroupMemberRepository groupMemberRepository;
+    private final GroupJoinRepository groupJoinRepository;
 
     public SearchGroupMemberListResponse searchAllGroupMembers(Long groupId) {
         // groupId로 그룹을 찾고 없으면 예외를 던짐
@@ -53,6 +56,7 @@ public class GroupMemberServiceImpl implements  GroupMemberSerivce{
                 .build();
     }
 
+    @Transactional
     public void joinGroupMember (GroupMemberRequest groupMemberRequest) {
         Member member = memberRepository.findById(groupMemberRequest.memberId())
                 .orElseThrow(() -> new IllegalArgumentException("멤버가 존재하지 않습니다."));
@@ -71,19 +75,28 @@ public class GroupMemberServiceImpl implements  GroupMemberSerivce{
                 .groups(groupJoin.getGroups())
                 .role("member")
                 .build();
+        groupJoinRepository.delete(groupJoin);
         groupMemberRepository.save(groupMember);
     }
 
+    @Transactional
     public void deleteGroupMember (GroupMemberRequest groupMemberRequest) {
         Member member = memberRepository.findById(groupMemberRequest.memberId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 멤버입니다."));
         Groups group = groupRepository.findById(groupMemberRequest.groupId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 그룹입니다."));
 
-        GroupMember groupMember = GroupMember.builder().member(member).groups(group).role("member").build();
-        groupMemberRepository.delete(groupMember);
+        List<GroupMember> groupMembers = member.getGroupMembers();
+
+        GroupMember targetGroupMember = groupMembers.stream()
+                .filter(groupMember -> groupMember.getGroups().getId().equals(group.getId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 그룹멤버가 없습니다."));
+
+        groupMemberRepository.delete(targetGroupMember);
     }
 
+    @Transactional
     public void changeRoleGroupMember (RollGroupMemberRequest rollGroupMemberRequest) {
         Groups group = groupRepository.findById(rollGroupMemberRequest.groupId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 그룹입니다."));
@@ -94,6 +107,9 @@ public class GroupMemberServiceImpl implements  GroupMemberSerivce{
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("해당 멤버는 그룹에 속해 있지 않습니다."));
 
-        targetGroupMember.setRole(rollGroupMemberRequest.role());
+        if (Objects.equals(rollGroupMemberRequest.role(), "up"))
+            targetGroupMember.setRole("admin");
+        else
+            targetGroupMember.setRole("member");
     }
 }
