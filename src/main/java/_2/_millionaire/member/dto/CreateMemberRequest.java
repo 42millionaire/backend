ee8e0a8742eca991eece14a2cbad6a2f3c6f0010
soohalee <suha630@naver.com>
2024-09-17 15:37:48 +0900

    [fix] : change class to record

--- src/main/java/_2/_millionaire/member/dto/CreateMemberRequest.java
+++ src/main/java/_2/_millionaire/member/dto/CreateMemberRequest.java
@@ -1,11 +1,8 @@
 package _2._millionaire.member.dto;
 
-import lombok.Getter;
+import lombok.Builder;
 
-@Getter
-public class CreateMemberRequest {
-
-    private String nickName;
-
-    private String email;
+@Builder
+public record CreateMemberRequest (String nickName,
+                                   String email) {
 }
