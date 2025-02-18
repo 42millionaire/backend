47396e0c93a037e5a1232fd5be2e1bb51c595d48
Hanjjong <tim4974@naver.com>
2025-02-18 17:25:28 +0900

    [fix] : modify GroupErrorCode

--- 42millionaire_spring/src/main/java/_2/_millionaire/group/exception/GroupErrorCode.java
+++ 42millionaire_spring/src/main/java/_2/_millionaire/group/exception/GroupErrorCode.java
@@ -6,7 +6,7 @@ public enum GroupErrorCode implements BaseErrorCode {
     GROUP_NOT_FOUND("존재하지 않는 그룹입니다.", 404),
     NOT_ADMIN_OF_GROUP("그룹의 관리자가 아닙니다.", 403),
 
-    ALREADY_JOIN_REQUEST_MEMBER("이미 해당 그룹에 가입 요청을 했습니다.", 416);
+    ALREADY_JOIN_REQUEST_MEMBER("이미 해당 그룹에 가입 요청을 했습니다.", 409);
 
     private final String message;
     private final int statusCode;
