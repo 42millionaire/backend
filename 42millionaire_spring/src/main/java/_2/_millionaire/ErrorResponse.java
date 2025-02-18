2f7ba7b986f0a6a5acdb411b3283020c0280cc2e
Hanjjong <tim4974@naver.com>
2025-02-18 17:46:21 +0900

    [fix] : modify ErrorResponse

--- 42millionaire_spring/src/main/java/_2/_millionaire/ErrorResponse.java
+++ 42millionaire_spring/src/main/java/_2/_millionaire/ErrorResponse.java
@@ -1,8 +1,10 @@
 package _2._millionaire;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class ErrorResponse {
     private String message;
     private int statusCode;
